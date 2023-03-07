bbe896c44b515e681222a1da2be1de570e69ca2e
Adilnurlan <nurlan.adil98@gmail.com>
2023-03-07 12:27:25 +0600

    end

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,11 @@
 import { Navigation } from '../Navigation/Navigation';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import s from './Layout.module.css';
 import Loader from 'components/Loader/Loader';
 
 const Layout = () => {
   return (
-    <div className={s.container}>
+    <div>
       <header>
         <Navigation />
       </header>
